de285e9d7b90ba16c721d078a883d4237cefb226
Daulet Tulepbergenov <daulet6368@gmail.com>
2025-01-03 16:53:11 +0500

    chore: up eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,22 @@
 import js from "@eslint/js";
+import importPlugin from "eslint-plugin-import";
 import perfectionist from "eslint-plugin-perfectionist";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import reactPlugin from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
+import reactHooksExtra from "eslint-plugin-react-hooks-extra";
 import reactRefresh from "eslint-plugin-react-refresh";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+  {
+    ...importPlugin.flatConfigs.recommended,
+    rules: {
+      "import/no-unresolved": "off",
+    },
+  },
   eslintPluginPrettierRecommended,
   {
     ...reactPlugin.configs.flat.recommended,
@@ -29,9 +37,11 @@ export default tseslint.config(
       "react-refresh": reactRefresh,
       perfectionist,
       "simple-import-sort": simpleImportSort,
+      "react-hooks-extra": reactHooksExtra,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      "@typescript-eslint/consistent-type-imports": "error",
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
