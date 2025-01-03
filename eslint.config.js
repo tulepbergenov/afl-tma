76938b717f6b4247abc118ae56d841f94522e30c
Daulet Tulepbergenov <daulet6368@gmail.com>
2025-01-03 15:27:41 +0500

    chore: add linters, alias, up vite config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,22 @@
 import js from "@eslint/js";
+import perfectionist from "eslint-plugin-perfectionist";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import reactPlugin from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  eslintPluginPrettierRecommended,
+  {
+    ...reactPlugin.configs.flat.recommended,
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+  { ignores: ["build"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
@@ -16,6 +27,8 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      perfectionist,
+      "simple-import-sort": simpleImportSort,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -24,5 +37,5 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
     },
-  }
+  },
 );
